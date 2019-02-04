6f97dd1c737a7e2de6295e45789c502024fa58d0
Kapilakshi <Kapilakshi@incedoinc.com>
2019-02-04 11:41:57 +0530

    changes for production interpret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
- // baseUrl: 'http://ailab001.incedoinc.com:9200/investopedia/_doc/_search?pretty',
-    baseUrl: 'http://10.11.198.208:9200/investopedia/_doc/_search?pretty',
-  // path: '/incedo-ameritrade-demo/grapherUI/'
-  path: '/angularnew/'
+  baseUrl: 'http://ailab001.incedoinc.com:9200/investopedia/_doc/_search?pretty',
+   // baseUrl: 'http://10.11.198.208:9200/investopedia/_doc/_search?pretty',
+   path: '/incedo-ameritrade-demo/grapherUI/interpret/'
+  //path: '/angularnew/'
 };
