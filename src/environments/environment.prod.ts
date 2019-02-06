4afae3251eb4d832a75e170c0960a5f1b8d6348e
Kapilakshi <Kapilakshi@incedoinc.com>
2019-02-06 16:03:38 +0530

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ailab001.incedoinc.com:9200/investopedia/_doc/_search?pretty',
+  baseUrl: 'http://ailab001.incedoinc.com:9200/investopedia_208/_doc/_search?pretty',
     //baseUrl: 'http://10.11.198.208:9200/investopedia/_doc/_search?pretty',
   path: '/incedo-ameritrade-demo/grapherUI/interpret/'
   //path: '/angularnew/'
